880fd964d7a00889120c6d60b40f4240b6c7ef02
Ceejay-777 <covenantcrackslord01@gmail.com>
2022-11-24 15:04:48 +0100

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// $('document').ready(function () {
+//     $('.container').mouseenter(function() {
+//         $('.frontface').css('display', 'none');
+//         $('.backface').css('display', 'block');
+//     });
+//     $('.container').mouseleave(function() {
+//         $('.frontface').css('display', 'block');
+//         $('.backface').css('display', 'none');
+//     });
+// })
